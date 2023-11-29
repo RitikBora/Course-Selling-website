b81c77bf4826ea941f2de661101b6aeeae104246
RitikBora <ritik.bora@xecurify.com>
2023-11-29 17:19:28 +0530

    mongo cloud connection testing

--- course_app/apps/client/src/pages/api/db/mongoose.ts
+++ course_app/apps/client/src/pages/api/db/mongoose.ts
@@ -1,8 +1,6 @@
 
 import mongoose from 'mongoose';
-
-
-const dbUrl = 'mongodb://127.0.0.1:27017/courseapp' ;
+import {config} from 'dotenv'
 
 let dbConnected = false;
 
@@ -11,6 +9,14 @@ export const checkifDbConnected = () =>
 
   if(!dbConnected)
   {
+    config();
+
+    const username = process.env.MONGO_USERNAME;
+    const password = process.env.MONGO_PASSWORD;
+    // const dbUrl = `mongodb+srv://${username}:${password}@projects.icbkpjd.mongodb.net/courseapp` ;
+    const dbUrl = 'mongodb://127.0.0.1:27017/courseapp' ; // for local testing
+
+    
     mongoose.connect(dbUrl);
     mongoose.connection.on('connected', () => {
       console.log('Connected to MongoDB');
